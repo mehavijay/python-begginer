bed5c60aea84403f2ce1f6e15148dcd17dcd62cb
mehavijay <32951398+mehavijay@users.noreply.github.com>
2017-10-21 11:33:46 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse023
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse023 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+counter=100
+miles  =1000.0
+name   ="John"
+print counter
+print miles
+print name
