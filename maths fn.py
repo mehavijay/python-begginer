97754882d461b9dd9cc76f5146514ba70c90ac0a
mehavijay <32951398+mehavijay@users.noreply.github.com>
2017-10-21 11:24:19 +0530

    Add files via upload

---
+++ maths fn.py
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse023
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse023 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=5
+b=6
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a**b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
